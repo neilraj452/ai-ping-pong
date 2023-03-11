1a17c1bc594211c011393290ced56387036dc3b0
neilraj452 <77378815+neilraj452@users.noreply.github.com>
2023-03-11 14:02:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,13 +23,23 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
-  canvas.parent("parent")
+  canvas.parent("parent");
+  video = createCapture(VIDEO);
+  video.size(700,600);
+  video.hide();
+  poseNet = ml5.poseNet(video,modelLoaded);
+  poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+  console.log("modal loaded");
 }
 
 
 function draw(){
 
  background(0); 
+  image(video,0,0,700,600);
 
  fill("black");
  stroke("black");
